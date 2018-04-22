4fd97ca98c942f78085981a22c522b665ca549df
Mohammad Askari <32340808+pyrrhicman@users.noreply.github.com>
2018-04-22 20:28:12 +0300

    fg

---
+++ mlcd_CPU/mlcd_CPU/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mlcd_CPU.cpp
+ *
+ * Created: 4/21/2018 10:12:21 PM
+ * Author : Mohammad
+ */ 
+
+#include <avr/io.h>
+//#include "D:\GitHub\MYLCDLIB\Lib\mlcd.cpp"
+//#include "D:\GitHub\MYLCDLIB\Lib\mlcd.h"
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
